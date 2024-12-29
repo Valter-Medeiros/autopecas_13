a96e55c5102f34783756e88b38657cc44163c2a7
Valter-Medeiros <mdrsvalter@hotmail.com>
2024-12-29 18:53:53 -0300

    Delete sair.php

--- sair.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); //começa uma nova sessão
-unset($_SESSION['email']);
-unset($_SESSION['senha']);//limpa a variável da sessão
-header('location: login.php');//manda para o login
-?>
